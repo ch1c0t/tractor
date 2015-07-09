befafc38732514071ccaf20fedb59b310ef4b6ea
Anatoly Chernow <f0ck1ng.namespace@gmail.com>
2015-07-09 23:38:47 +0300

    Initial commit

---
+++ lib/tractor.rb
@@ -0,0 +1,46 @@
+require 'redis'
+
+module Tractor
+  class << self
+    def ipc
+      setup_redis
+      patch_Integer_shamelessly
+    end
+
+    private
+
+    def setup_redis
+      stdout = $stdout.clone
+      $stdout.reopen (File.new '/dev/null', 'w')
+
+      redis_pid = spawn 'redis-server --port 0 --unixsocket tractor.sock'
+      parent_pid = Process.pid
+      at_exit { `kill #{redis_pid}` if Process.pid == parent_pid }
+
+      const_set :R, (Redis.new path: 'tractor.sock')
+
+      20.times do
+        begin
+          break if R.ping == 'PONG'
+          sleep 0.01
+        rescue Errno::ENOENT
+          next
+        end
+      end
+
+      $stdout = stdout
+    end
+
+    def patch_Integer_shamelessly
+      Integer.class_eval do
+        def put message
+          R.rpush self, message
+        end
+
+        def get
+          (R.blpop [self])[1]
+        end
+      end
+    end
+  end
+end
---
+++ tractor.gemspec
@@ -0,0 +1,8 @@
+Gem::Specification.new do |g|
+  g.name    = 'tractor'
+  g.version = '0.0.0'
+  g.summary = 'An Erlang-style messaging for Ruby processes.'
+  g.authors = ['Anatoly Chernow']
+
+  g.add_dependency 'redis'
+end
